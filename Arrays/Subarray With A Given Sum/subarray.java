a2d42b9bb99427efec779c4033014307611c8f43
arjunm052 <singh.arjun127@gmail.com>
2021-06-02 21:54:29 +0530

    Subarray Sum Final

--- Arrays/Subarray With A Given Sum/subarray.java
+++ Arrays/Subarray With A Given Sum/subarray.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class subarray {
-    public static int subArraySumPositive(int[] arr,int n, int sum){
+    public static int subArraySumPositive(int[] arr, int sum){
         int start = 0;
         int currSum = arr[0];
 
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=arr.length;i++){
             while(currSum > sum && start < i-1){
                 currSum -= arr[start++];             
             }
@@ -16,7 +16,7 @@ public class subarray {
                 return 1;
             }
 
-            if(i<n){
+            if(i<arr.length){
                 currSum+=arr[i];
             }
         }
@@ -25,13 +25,42 @@ public class subarray {
         return -1;
     }
 
+    public static int subArraySumNegative(int[] arr, int sum) {
+    int count = 0, currSum = 0;
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1);
+        for (int i = 0; i < arr.length; i++) {
+            currSum += arr[i];
+            if (map.containsKey(currSum - sum)){
+                count += map.get(currSum - sum);
+            }
+            map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+        }
+        if(count>0){
+            System.out.println("No of subarrays is " + count);
+            return count;
+        }
+            System.out.println("No subarrays present");
+            return -1;
+    }
+
+    
+
     public static void main(String[] args) {
         int[] arr1 = {1,2,3,7,5};
-        int n1 = 5, sum1 = 12;
-        subArraySumPositive(arr1,n1,sum1);
+        int sum1 = 12;
+        // subArraySumPositive(arr1,sum1);
+        subArraySumNegative(arr1, sum1);
+        
         
         int[] arr2 =  {1,2,3,4,5,6,7,8,9,10};
-        int n2=10,sum2=15;
-        subArraySumPositive(arr2, n2, sum2);
+        int sum2=15;
+        // subArraySumPositive(arr2, sum2);
+        subArraySumNegative(arr2, sum2);
+
+        int[] arr3 = {3,4,7,2,-3,1,4,2};
+        int sum3 = 7;
+        subArraySumNegative(arr3, sum3);
+       
     }
 }
