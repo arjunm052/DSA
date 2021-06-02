7f36d05409f3d4fbae059fb273a0f30f31524711
arjunm052 <singh.arjun127@gmail.com>
2021-06-02 23:03:41 +0530

    Final on Triplet Count from lappy

--- Arrays/Count Triplets in Array/triplets.java
+++ Arrays/Count Triplets in Array/triplets.java
@@ -1,33 +1,30 @@
+
 //Given an array of DISTINCT integers. The task is to count all the triplets such that sum of two elements equals the third element.
 import java.util.*;
 
 public class triplets {
 
     public static int countTriplet(int[] arr) {
-        int countTriplet(int arr[], int n) {
-       HashSet<Integer> set = new HashSet<>();
-       for (int i=0;i<arr.length;i++){
-           set.add(arr[i]);
-       }
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
 
-       int count = 0;
-        for(int i=0;i<arr.length-1;i++){
-           for(int j=i+1;j<arr.length;j++){
-                if(set.contains(arr[i]+arr[j])){
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (set.contains(arr[i] + arr[j])) {
                     count++;
                 }
-             }
+            }
         }
-            System.out.println("No. of triplets is " + count);
-            return count;
+        System.out.println("No. of triplets is " + count);
+        return count;
     }
 
-
-
-
     public static void main(String[] args) {
-        int[] arr1 = {1, 5, 3, 2};
-        int[] arr2 = {2, 3, 4};
+        int[] arr1 = { 1, 5, 3, 2 };
+        int[] arr2 = { 2, 3, 4 };
         countTriplet(arr1);
         countTriplet(arr2);
     }
