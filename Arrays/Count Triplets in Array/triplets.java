49cbd5a5272fff4e425881f4a956d0d4d7cb0087
arjunm052 <singh.arjun127@gmail.com>
2021-06-02 22:56:30 +0530

    test from lappy

--- Arrays/Count Triplets in Array/triplets.java
+++ Arrays/Count Triplets in Array/triplets.java
@@ -1,4 +1,4 @@
-//Given an array of distinct integers. The task is to count all the triplets such that sum of two elements equals the third element.
+//Given an array of DISTINCT integers. The task is to count all the triplets such that sum of two elements equals the third element.
 import java.util.*;
 
 public class triplets {
@@ -9,21 +9,21 @@ public class triplets {
        for (int i=0;i<arr.length;i++){
            set.add(arr[i]);
        }
-       
+
        int count = 0;
         for(int i=0;i<arr.length-1;i++){
            for(int j=i+1;j<arr.length;j++){
                 if(set.contains(arr[i]+arr[j])){
                     count++;
-                } 
+                }
              }
         }
             System.out.println("No. of triplets is " + count);
             return count;
     }
-        
 
-    
+
+
 
     public static void main(String[] args) {
         int[] arr1 = {1, 5, 3, 2};
