05789f0619b0417cc94efe2aa5613f09027cd1b0
arjunm052 <singh.arjun127@gmail.com>
2021-08-13 15:03:50 +0530

    Dijkstras Algorithm

---
+++ Graphs/Adjacency List /Dijkstras Algorithm/MGraph.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class MGraph {
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        Edge(int src,int dest,int wt){
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }      
+    }
+
+    public static class Pair implements Comparable<Pair>{
+        int v;
+        String psf;
+        int wsf;
+
+        Pair(int v,String psf,int wsf){
+            this.v = v;
+            this.psf = psf;
+            this.wsf=wsf;
+        }
+
+        public int compareTo(Pair o) {  
+            return (this.wsf - o.wsf);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of Vertices");
+        int v = sc.nextInt();
+        System.out.println("Enter no. of edges");
+        int e = sc.nextInt();
+
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        for(int i=0;i<v;i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        System.out.println("Enter the edges: ");
+        for(int i=0;i<e;i++){
+            int src = sc.nextInt();
+            int dest = sc.nextInt();
+            int wt = sc.nextInt();
+            graph[src].add(new Edge(src,dest,wt));
+            graph[dest].add(new Edge(dest,src,wt));
+        }
+
+        System.out.println("Enter source: ");
+        int source = sc.nextInt(); 
+        sc.close();
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[v];
+        pq.add(new Pair(source,source+"",0));
+
+
+        while(pq.size()>0){
+            Pair rem = pq.remove();
+
+            if(visited[rem.v]){
+                continue;
+            }
+
+            visited[rem.v] = true;
+            System.out.println(rem.v + " via " + rem.psf + "@" + rem.wsf);
+
+            for(Edge edge: graph[rem.v]){
+                if(!visited[edge.dest]){
+                    pq.add(new Pair(edge.dest,rem.psf+edge.dest,rem.wsf+edge.wt));
+                }
+            }
+        }
+    }
+
+
+}
